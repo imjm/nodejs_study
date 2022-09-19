2b13c0fc560b7cae47966fe3d21243529b999832
imjm <77835002+imjm@users.noreply.github.com>
2022-09-19 13:14:49 +0900

    studied in YOUTUBE

---
+++ node_study/syntax/String.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('Hello my name is jeong min'.length);
---
+++ node_study/syntax/function.js
@@ -0,0 +1,6 @@
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+function sum(first, second) {
+    return first + second;
+}
+sum(2, 4);

fd38db02e4dfd7e2feb80bdf039569a3c08cfdcc
imjm <77835002+imjm@users.noreply.github.com>
2022-10-09 16:07:10 +0900

    express에서 모든 것은 미들웨어로 이루어져 있다

--- node_study/main.js
+++ node_study/main.js
@@ -14,42 +14,45 @@ var compression = require('compression');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
+app.use(function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next();
+  });
+});
 
 app.get('/', function(request, response) {
-  fs.readdir('./data', function(error, filelist){
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = tp.list(filelist);
-    var html = tp.html(title, list, 
-      `<h2>${title}</h2>${description}`, 
-      '<a href="/create">create</a>');
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = tp.list(request.list);
+  var html = tp.html(title, list, 
+    `<h2>${title}</h2>${description}`, 
+    '<a href="/create">create</a>');
   response.send(html);
-  });    
 });
 
 app.get('/page/:pageId', function(request, response) {
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = tp.list(filelist);
-      var html = tp.html(title, list,
-         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-         `<a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>
-          `);
-    response.send(html);
+  
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    var title = request.params.pageId;
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description, {
+      allowedTags:['h1']
+    });
+    var list = tp.list(request.list);
+    var html = tp.html(title, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        `<a href="/create">create</a>
+        <a href="/update/${sanitizedTitle}">update</a>
+        <form action="/delete_process" method="post">
+          <input type="hidden" name="id" value="${sanitizedTitle}">
+          <input type="submit" value="delete">
+        </form>
+        `);
+  response.send(html);
     });
   });
- });
 
  app.get('/create', function(request, response){
   fs.readdir('./data', function(error, filelist){
